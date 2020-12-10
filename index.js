62651aabbff4e2964bc4cc82319227c911761a63
anberjamz <anberjam@yahoo.com>
2020-12-09 20:56:15 -0500

    commit tree 84a1fdf75f42ec70038e2eb9e4b8ab51f35d39e8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob"
+var bestCustomer;
+var leastFavoriteCustomer = "Saif"
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Me"
+}
+
